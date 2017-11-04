02fc067b4ef73412c12675a444decc76f678d8b8
Jason Chang <pyo0220@gmail.com>
2017-11-03 17:20:26 -0700

    plot bayes factors instead

--- chipt_stability.py
+++ chipt_stability.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rc('text', usetex=True)
 
 f = open('./data/stability.csv','r')
 next(f)
@@ -15,6 +16,9 @@ ticks = []
 chisq_max = 0
 lgbf_max = 0
 y = 0
+lgbf_list = []
+avg_list = []
+label_list = []
 for l in f:
     if l.split()[0] == 'break':
         y -= 1
@@ -28,6 +32,8 @@ for l in f:
         else:
             avg = False
         label = dat[1]
+        avg_list.append(avg)
+        label_list.append(label)
         #print label
         mean = float(dat[2])
         sdev = float(dat[3])
@@ -35,7 +41,8 @@ for l in f:
         if chi2dof > chisq_max:
             chisq_max = chi2dof
         logGBF = float(dat[5])
-        if logGBF > lgbf_max:
+        lgbf_list.append(logGBF)
+        if logGBF > lgbf_max and logGBF not in [10000]:
             lgbf_max = logGBF
         #if label=='NNNLO $\chi$PT':
         #    ax0.errorbar(x=mean,xerr=sdev,y=y,ls='None',marker='s',fillstyle='full',markersize='5',elinewidth=1,capsize=2,color='black',mew=1)
@@ -55,36 +62,54 @@ for l in f:
             mrk = 's'
             clr = 'k'
             mfc = 'k'
-            lbl = r'\textbf{weighted avg}'
+            lbl = r"\textbf{model avg}"
         else:
             clr = '#b36ae2'
         ax0.errorbar(x=mean,xerr=sdev,y=y,ls='None',marker=mrk,mfc=mfc,markersize='5',elinewidth=1,capsize=2,color=clr,mew=1)
         ax1.errorbar(x=chi2dof,y=y,ls='None',marker=mrk,mfc=mfc,markersize='5',elinewidth=1,capsize=2,color=clr,mew=1)
-        ax2.errorbar(x=logGBF,y=y,ls='None',marker=mrk,mfc=mfc,markersize='5',elinewidth=1,capsize=2,color=clr,mew=1)
+        #ax2.errorbar(x=logGBF,y=y,ls='None',marker=mrk,mfc=mfc,markersize='5',elinewidth=1,capsize=2,color=clr,mew=1)
         ticklabels.append(lbl)
+bf = np.exp(np.array(lgbf_list)-np.array(lgbf_max))
+print(bf)
+for idx,t in enumerate(ticks):
+    if avg_list[idx]:
+        mfc = '#b36ae2'
+        #lbl = r'\textbf{%s}' %label
+        mrk = 's'
+    else:
+        mfc = 'None'
+    if label_list[idx]=='weighted avg':
+        mrk = 's'
+        clr = 'k'
+        mfc = 'k'
+        lbl = r"\textbf{model avg}"
+    else:
+        clr = '#b36ae2'
+    ax2.errorbar(x=bf[idx],y=t,ls='None',marker=mrk,mfc=mfc,markersize='5',elinewidth=1,capsize=2,color=clr,mew=1)
+
 #print ticklabels
-ax0.set_xlabel('$g_A$', fontsize=20)
-ax1.set_xlabel('$\chi^2/$dof', fontsize=20)
-ax2.set_xlabel('logGBF', fontsize=20)
+ax0.set_xlabel('$g_A$', fontsize=16)
+ax1.set_xlabel('$\chi^2/$dof', fontsize=16)
+ax2.set_xlabel('Bayes Factor', fontsize=16)
 ax0.set_yticks(ticks)
 ax0.set_yticklabels(ticklabels)
 ax1.set_yticks(ticks)
 ax2.set_yticks(ticks)
 ax0.set_xticks([1.2,1.24,1.28,1.32,1.36])
-ax1.set_xticks([0.5,1])
-ax2.set_xticks([0,10,20])
+ax1.set_xticks([0.4,1])
+ax2.set_xticks([0,0.5,1])
 ax0.set_ylim([ticks[-1]-1,ticks[0]+1])
 ax0.set_xlim([1.2,1.35])
 ax1.set_ylim([ticks[-1]-1,ticks[0]+1])
 ax1.set_xlim([0,chisq_max*1.1])
 ax2.set_ylim([ticks[-1]-1,ticks[0]+1])
-ax2.set_xlim([0,lgbf_max*1.1])
-ax0.xaxis.set_tick_params(labelsize=16)
-ax1.xaxis.set_tick_params(labelsize=16)
-ax2.xaxis.set_tick_params(labelsize=16)
+ax2.set_xlim([-0.1,1.1])
+ax0.xaxis.set_tick_params(labelsize=12)
+ax1.xaxis.set_tick_params(labelsize=12)
+ax2.xaxis.set_tick_params(labelsize=12)
 ax0.yaxis.set_tick_params(labelsize=10)
 ax1.yaxis.set_tick_params(labelleft=False)
 ax2.yaxis.set_tick_params(labelleft=False)
 plt.draw()
-plt.savefig('chipt_stability.pdf',transparent=True)
+plt.savefig('./plots/chipt_stability.pdf',transparent=True)
 plt.show()
