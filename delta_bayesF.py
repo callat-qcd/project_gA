aa0f68d6c4bc0cd3604ab065b38aeb14ed5f72ce
Jason Chang <pyo0220@gmail.com>
2017-11-07 22:41:58 -0800

    update bayes plot

--- delta_bayesF.py
+++ delta_bayesF.py
@@ -5,7 +5,7 @@ plt.rc('text', usetex=True)
 f = open('./data/delta_fits.csv','r')
 next(f)
 fig = plt.figure('delta Bayes Factors',figsize=(7,4.326237))
-ax0 = plt.axes([0.1,0.15,0.85,0.8])
+ax0 = plt.axes([0.15,0.15,0.8,0.8])
 lgbf_max = 0
 lgbf_list = []
 x = []
@@ -19,11 +19,13 @@ for l in f:
 lgbf_list = np.array(lgbf_list)
 x = np.array(x)
 bf = np.exp(np.array(lgbf_list)-np.array(lgbf_max))
-ax0.plot(x,bf,marker='o')
-ax0.plot(x[np.argmax(bf)],bf[np.argmax(bf)],marker='s',color='k')
+ax0.plot([i for idx,i in enumerate(x) if idx not in [np.argmax(bf)]],[i for idx,i in enumerate(bf) if idx not in [np.argmax(bf)]],marker='s',fillstyle='none',markersize='5',color='#51a7f9')
+ax0.plot(x[np.argmax(bf)],bf[np.argmax(bf)],marker='*',color='#c82506',fillstyle='full',markersize='8')
 
-ax0.set_ylabel(r'Bayes Factor', fontsize=16)
-ax0.set_xlabel(r'\% prior width', fontsize=16)
+ax0.set_ylabel(r'Bayes factor', fontsize=20)
+ax0.set_xlabel(r'prior width (\% of central value)', fontsize=20)
+ax0.xaxis.set_tick_params(labelsize=16)
+ax0.yaxis.set_tick_params(labelsize=16)
 plt.draw()
 plt.savefig('./plots/delta_BF.pdf',transparent=True)
 plt.show()
